bc2018da010378377f5a6aafd8ad51fafe0dd31a
Afrânio Segundo <113372496+Sigundin@users.noreply.github.com>
2023-03-03 12:13:33 -0300

    Avaliacao interativa2

---
+++ main.js
@@ -0,0 +1,10 @@
+    const esconde = document.getElementById("menuDeAvaliacao")
+    const elementoObrigado = document.getElementById("avaliacaoCompleta")
+    const nota = document.querySelectorAll(".numero")
+    const elementoAvaliacao = document.getElementById('selecinado')
+
+function esconder(){
+    esconde.style.display = "none"
+    elementoObrigado.style.display = "block"
+    
+}
